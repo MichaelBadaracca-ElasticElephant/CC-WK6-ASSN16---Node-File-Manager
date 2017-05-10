12e3f8df95ad870ea0ba8bcad463cfc515012d74
Michael Badaracca - Elastic Elephant <michael.badaracca@elasticElephant.com>
2017-05-09 23:55:43 -0600

    Added comments. Added confirmatin on delation. Added prompt module to help confirm deletion

--- file-manager.js
+++ file-manager.js
@@ -1,5 +1,6 @@
 // Include fs module
 var fs = require("fs");
+var prompt = require("prompt");
 
 function useStdin() {
 	var input = process.stdin.read();
@@ -8,8 +9,6 @@ function useStdin() {
 		return;
 	}
 
-	// console.log(input.toString());
-
 	// convert to string, trim whitespace, split to array of words
 	var inputSplit = input.toString().trim().split(" ");
 
@@ -17,11 +16,18 @@ function useStdin() {
 		 catFile(inputSplit[1]);
 	} else if (inputSplit[0] === "touch") {
 		touchFile(inputSplit[1]);
+	} else if (inputSplit[0] === "rm"){
+		removeFile(inputSplit[1]);
+	} else if (inputSplit[0] === "replace"){
+		replacePhrase(inputSplit[1],inputSplit[2],inputSplit[3])
+	} else if(inputSplit[0] === "grep"){
+		findLinesInFile(inputSplit[1], inputSplit[2]);
 	}
 }
 
 process.stdin.on("readable", useStdin);
 
+//logs the contents of the specified file
 function catFile(fileName) {
 	fs.readFile(fileName, function(err, data) {
 		if (err) {
@@ -32,6 +38,7 @@ function catFile(fileName) {
 	});
 }
 
+//Appends nothing to file and logs message
 function touchFile(fileName) {
 	fs.appendFile(fileName, "", function(err) {
 		if (err) {
@@ -42,6 +49,78 @@ function touchFile(fileName) {
 		console.log("Touched file!");
 	});
 }
+
+//Removes the specified file
+function removeFile(fileName){
+	//prompt user to confirm deletion
+	console.log("Are you sure you want to delete this file (y/n)?:")
+	prompt.get(["confirm"],function(error, result){
+		if(result.confirm === 'y'){
+			fs.unlink(fileName,function(err) {
+				if (err) {
+					console.log(err);
+					return;
+				}
+			});
+			console.log("Removed " + fileName);
+		}
+		else{
+			console.log("Did not remove file");	
+		}
+
+	});
+
+}
+
+//Searches through a specified file and replaces a phrase if it is found
+function replacePhrase(fileName, phraseToReplace, replacementPhrase){
+	//Read file in and save to var, replace phrase in file, rewrite file
+	fs.readFile(fileName, function(err, data) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		//Replace the phrase
+		var updatedFileData = data.toString().replace(phraseToReplace, replacementPhrase);
+		//Update the file by overwriting it with new data
+		fs.writeFile(fileName,updatedFileData, function(err, data) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+		});
+		console.log("Replaced " + phraseToReplace + " with " + replacementPhrase);
+	});
+}
+
+//Find the line numbers where a phrase exists in a specified file
+function findLinesInFile(fileName, phraseToFind){
+	//load in the file contents
+	fs.readFile(fileName, function(err, data) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		//break file data into lines
+		var linesOfFile = data.toString().split('\n');
+
+		//loop through each line and search for phrase, if found, note line number
+		for(var lineCount = 0; lineCount < linesOfFile.length; lineCount++){
+			if(linesOfFile[lineCount].search(phraseToFind) !== -1){
+				console.log(lineCount + 1);
+			}
+		}		
+	});
+}
+
+function errorHandler(err){
+	if (err) {
+		console.log(err);
+		return;
+	}
+}
+
+
 
 /*
 Your assignment is to implement the following functionality:
